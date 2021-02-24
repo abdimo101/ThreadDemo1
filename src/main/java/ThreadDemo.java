af2d9f13bd2126a92bc1405d29f480b1abdd8720
Abdi Abdulle <abdi.abdulle@outlook.dk>
2021-02-24 09:47:15 +0100

    Initial commit

--- src/main/java/ThreadDemo.java
+++ src/main/java/ThreadDemo.java
@@ -1,27 +1,22 @@
 class MyRunnable implements Runnable{
     public void run() {
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello from Thread" + Thread.currentThread().getName() + " i = "+i);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        System.out.println("Hello");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         }
     }
-}
+
 
 public class ThreadDemo {
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(new MyRunnable());
-        Thread t2 = new Thread(new MyRunnable());
         t1.start();
-        t2.start();
+
 
         t1.join();
-        t2.join();
-        System.out.println("Who am I? " + Thread.currentThread().getName());
         System.out.println("Are we done?");
     }
 }
